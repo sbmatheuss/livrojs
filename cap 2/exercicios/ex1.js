754cd58ccc2223e9cb7d04837518488bd327b9a0
Matheus Braga <sbmatheus20@gmail.com>
2025-08-27 19:44:56 -0300

    Calculo da promoção por quantidade de medicamento

--- cap 2/exercicios/ex1.js
+++ cap 2/exercicios/ex1.js
@@ -1,19 +1,32 @@
 // Cria uma referência ao form e aos elementos h3 (onde será exibida a resposta)
 
 const frm = document.querySelector("form")
-const resp1 = document.querySelector("#outResp1")
-const resp2 = document.querySelector("#outResp2")
+const resp2 = document.querySelector("#resp2")
 
 // Cria um "ouvinte" de evento, acionado quando o botão submit for clicado
 
 frm.addEventListener("submit", (e) => {
+  e.preventDefault()
 
-  const medi = Number(frm.inMedi.value) // obtém conteúdo dos campos
+  const qtd = Number(frm.inQtd.value) // obtém conteúdo dos campos
   const preco = Number(frm.inPreco.value)
 
-  const promo = 
-  resp1.innerText = `Promoção ${promo}`
+  let total = 0;
+  let msg = " ";
+
+  if (qtd >= 2) {
+    const precoDesc = Math.floor(preco);
+    total = precoDesc * qtd;
+
+    msg = `Leve 2 por apenas R$: ${total.toFixed(2)}`;
+    
+  } else {
+
+    total = preco * qtd;
+    msg = `Total a pagar R$: ${total.toFixed(2)}`
+  }
 
 
-  e.preventDefault()
+  resp2.textContent = msg
+  
 })
