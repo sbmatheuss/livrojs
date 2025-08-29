e934cb82064dc7648e3e7957d66f71f2a27b2b25
Matheus Braga <sbmatheus20@gmail.com>
2025-08-28 22:26:22 -0300

    criando exercicio 2

---
+++ cap 2/exercicios/02/ex2.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("form")
+const resp2 = document.querySelector("#resp")
+
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const valor = Number(frm.inValor.value);
+  const time = Number(frm.inTime.value)
+
+  let s = 0;
+  let msg = " ";
+
+  if (valor >= 3) {
+      const time = 
+      s = valor * 2
+  }
+})
