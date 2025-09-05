966b9e4dbd2f91a3931b0fbcdb50689137739c52
Matheus Braga <sbmatheus20@gmail.com>
2025-09-04 22:41:37 -0300

    resolvendo esses exercicios atraves do terminal

---
+++ cap3/soma.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+// Entrada de dados
+const num1 = Number(prompt("1 Numero: "));
+const num2 = Number(prompt("2 Numero: "));
+const soma = num1 + num2
+console.log(`Soma é: ${soma}`)
+
+
