680cd26f4d9a8ce6f5223b0d0f93d9707917056c
Matheus Braga <sbmatheus20@gmail.com>
2026-02-19 15:03:05 -0300

    inclusão e exclusão de itens

---
+++ cap6/exemplos/exp01.js
@@ -0,0 +1,15 @@
+const cidades = ["Pelotas"]
+
+cidades.push("São Lourenço")
+console.log(cidades)
+
+
+cidades.unshift("Porto Alegre")
+console.log(cidades)
+
+const ultima = cidades.pop()
+console.log(cidades)
+
+const primeira = cidades.shift()
+console.log(cidades)
+
