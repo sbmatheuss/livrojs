0fea3eaa3cc88182f2665318e898bf4e7b1b8312
Matheus Braga <sbmatheus20@gmail.com>
2026-02-19 15:27:06 -0300

    vetores

---
+++ cap6/exemplos/exp02.js
@@ -0,0 +1,11 @@
+const letras = ["A", "B", "C", "D"];
+const letras2 = letras.slice(-2)
+const letras3 = letras.slice(0, -1)
+console.log(letras)
+console.log(letras2)
+console.log(letras3)
+
+const retira = letras.splice(1, 2)
+console.log(letras)
+console.log(retira)
+
