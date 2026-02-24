19fcbef1bdb34f4b3b0324189581a7b035d1d215
Matheus Braga <sbmatheus20@gmail.com>
2026-02-23 21:14:22 -0300

    indexOf, lasIndexOf, includes

---
+++ cap6/exemplos/04/script.js
@@ -0,0 +1,7 @@
+const idades = [5, 6, 8, 3, 6, 9]
+
+console.log(idades.indexOf(6)) // 1
+console.log(idades.lastIndexOf(6)) // 4
+console.log(idades.indexOf(7)) // -1
+console.log(idades.includes(3)) // true
+
