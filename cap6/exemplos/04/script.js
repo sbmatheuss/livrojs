99f1390ba2690038769f2fa8344d222ac6f97753
Matheus Braga <sbmatheus20@gmail.com>
2026-02-24 20:30:27 -0300

    Jogo Descubra o Número

--- cap6/exemplos/04/script.js
+++ cap6/exemplos/04/script.js
@@ -4,6 +4,11 @@ console.log(idades.indexOf(6)) // 1
 console.log(idades.lastIndexOf(6)) // 4
 console.log(idades.indexOf(7)) // -1
 console.log(idades.includes(3)) // true
+
+
+
+
+
                  
 
   
--- cap6/exemplos/05/script.js
+++ cap6/exemplos/05/script.js
@@ -3,3 +3,42 @@ const respErros = document.querySelector("#outErros");
 const respoChances = document.querySelector("#outChances");
 const respDica = document.querySelector("#outDica");
 
+
+const erros = []
+const sorteado = Math.floor(Math.random() * 100) + 1
+const CHANCES = 6
+
+frm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const numero = Number(frm.inNumero.value)
+    if(numero == sorteado) {
+        respDica.innerText = `Parabéns! Número sorteado: ${sorteado}!`
+        frm.btSubmit.disabled = true
+        frm.btNovo.className = "exibe"
+    } else {
+        if(erros.includes(numero)) {
+            alert(`Você já apostou o número ${numero}. Tente outro!`)
+    }      else {
+            erros.push(numero)
+            const numErros = erros.length
+            const numChances = CHANCES - numErros
+
+            respErros.innerText = `${numErros} (${erros.join(", ")})`
+            respoChances.innerText = numChances
+
+            if(numChances == 0) {
+                alert(`Suas chances acabaram... `)
+                frm.btSubmit.disabled = true
+                frm.btNovo.className = "exibe"
+                respDica.innerText = `GAME OVER! Número sorteado: ${sorteado}.`
+            } else {
+                const dica = numero < sorteado ? "maior" : "menor"
+                respDica.innerText = `Dica: Tente um número ${dica} que ${numero}.`
+            }
+        }
+    }
+
+    frm.inNumero.value = ""
+    frm.inNumero.focus()
+} )
