a03cb530eac85610718ca06e3e2cb8823a4cc978
Matheus Braga <sbmatheus20@gmail.com>
2026-02-23 23:19:16 -0300

    estruturando javascript

---
+++ cap6/exemplos/05/script.js
@@ -0,0 +1,5 @@
+const frm = document.querySelector("form");
+const respErros = document.querySelector("#outErros");
+const respoChances = document.querySelector("#outChances");
+const respDica = document.querySelector("#outDica");
+
