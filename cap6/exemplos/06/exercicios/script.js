50c43aed835fc4ba41d10600c4a3ba13cf5e7aed
Matheus Braga <sbmatheus20@gmail.com>
2026-04-10 07:31:43 -0300

    feat: Criado o event Simulador de Promoçãor

--- cap6/exemplos/06/exercicios/script.js
+++ cap6/exemplos/06/exercicios/script.js
@@ -46,3 +46,21 @@ frm.btFiltrar.addEventListener("click", () => {
 })
 
 
+frm.btSimular.addEventListener("click", () => {
+    const desconto = Number(prompt ("Qual o percentual de desconto: "))
+    if(desconto == 0 || isNaN(desconto)) {
+        return
+    }
+
+    const carrosDesc = carros.map(aux => ({
+        modelo: aux.modelo,
+        preco: aux.preco - (aux.preco * desconto / 100)
+    }))
+
+    let lista = ""
+    for (const carro of carrosDesc) {
+        lista += `${carro.modelo} - R$: ${carro.preco.toFixed(2)}\n`
+    }
+    respo.innerText = `Carros com desconto: ${desconto}%\n${"-".repeat(40)}\n${lista}`
+})
+
