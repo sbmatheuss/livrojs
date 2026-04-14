1f14e4ed74b265512d12d3ff9a046a5f375e9163
Matheus Braga <sbmatheus20@gmail.com>
2026-04-14 17:59:10 -0300

    Feat: Fixando o método reverse

---
+++ cap6/exemplos/06/exercicios/reverse/convidado.js
@@ -0,0 +1,7 @@
+const convidados = ["Mateus", "Ana", "Wilson", "Bernardo", "Yandra"]
+
+const lista = [...convidados].sort()
+
+lista.reverse()
+
+console.log(lista)
---
+++ cap6/exemplos/06/exercicios/reverse/notas.js
@@ -0,0 +1,5 @@
+const notas = [7.5, 9.2, 10, 8.5, 9.2, 6.0]
+
+const descrescente = notas.sort((a, b) => b - a) 
+
+console.log(descrescente)
