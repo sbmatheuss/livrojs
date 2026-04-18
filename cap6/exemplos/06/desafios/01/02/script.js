88f6be9c01dfb68c9266ca84fa55308b348666a2
Matheus Braga <sbmatheus20@gmail.com>
2026-04-17 21:14:11 -0300

    Feat: Desafio concluido

--- cap6/exemplos/06/desafios/01/02/script.js
+++ cap6/exemplos/06/desafios/01/02/script.js
@@ -3,6 +3,7 @@ const numeros = []
 const inNumero = document.getElementById("inNumero")
 const outLista = document.getElementById("outLista")
 const btAdicionar = document.getElementById("btAdicionar")
+const VerificarOrdem = document.getElementById("btVerificar")
 
 function adicionarNumero() {
     
@@ -12,8 +13,25 @@ function adicionarNumero() {
         outResposta.innerText = "ATENÇÃO... ESTE NÚMERO JA FOI ADICIONADO!"
         return
     } else {
-        
+        numeros.push(num)
+        outLista.innerText = "Números: " + numeros.join(", ");
     }
 
+    
+
 }
+
+
+function btVerificar() {
+    for(let i = 0; i < numeros.length - 1; i++) {
+        if(numeros[i] > numeros[i+1]){
+            outResposta.innerText = "NÃO ESTÁ EM ORDEM CRESCENTE"
+            return
+        }
+    }
+
+    outResposta.innerText = "ESTÁ EM ORDEM CRESCENTE"
+}
+
 btAdicionar.addEventListener("click", adicionarNumero);
+VerificarOrdem.addEventListener("click", btVerificar)
