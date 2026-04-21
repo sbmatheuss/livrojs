6b63d2bf7a7b9be5f358e94c7496b2d0d8dfa0d5
Matheus Braga <sbmatheus20@gmail.com>
2026-04-21 11:55:20 -0300

    Cópia de Caracteres e Remoção de espaços da String

---
+++ cap7/02/exemplos/04/script.js
@@ -0,0 +1,21 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = frm.inFuncionario.value.trim()
+
+    if(!nome.includes(" ")){
+        alert("Informe o nome completo...")
+        return
+    }
+
+    const priEspaco = nome.indexOf(" ")
+    const ultEspaco = nome.lastIndexOf(" ")
+
+    const cracha = nome.substr(0, priEspaco) + nome.substr(ultEspaco)
+
+
+    resp.innerText = `Crachá: ${cracha}`
+})
