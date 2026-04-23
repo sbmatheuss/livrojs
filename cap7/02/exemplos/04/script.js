fccb88354baaeed056259bba70db796dfcb75b01
Matheus Braga <sbmatheus20@gmail.com>
2026-04-22 22:42:58 -0300

    Add: Exemplo 04 - Palavras invertidas com js

--- cap7/02/exemplos/04/script.js
+++ cap7/02/exemplos/04/script.js
@@ -2,20 +2,38 @@ const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault()
+  const senha = frm.inSenha.value
+  const erros = []
 
-    const nome = frm.inFuncionario.value.trim()
+  // Verifica se o tamanho da senha e valido
+  if(senha.length < 8 || senha.length > 15){
+    erros.push("possuir entre 8 e 15 caracteres")
+  }
 
-    if(!nome.includes(" ")){
-        alert("Informe o nome completo...")
-        return
+  // Verifica se nao possui numeros
+  if(senha.match(/[0-9]/g) == null) {
+    erros.push("possuir numeros (no minimo, 1)")
+  }
+
+  // Verifica se nao possui letras minusculas
+  if(!senha.match(/[a-z]/g)) {
+    erros.push("possuir letras mainusculas (no minimo, 1)")
+  }
+
+  // Verifica se nao letras maiusculas ou se possui apenas 1
+  if(!senha.match(/[A-Z]/g) || senha.match(/[A-Z]/g).length == 1) {
+    erros.push("possuir letras maiusculas (no minimo, 2)")
+  }
+
+  // Verifica se nao possui simbolos ou "_"
+  if(!senha.match(/[\W|_]/g)) {
+      erros.push("possuir simbolos (no minimo, 1)")
     }
 
-    const priEspaco = nome.indexOf(" ")
-    const ultEspaco = nome.lastIndexOf(" ")
-
-    const cracha = nome.substr(0, priEspaco) + nome.substr(ultEspaco)
-
-
-    resp.innerText = `Crachá: ${cracha}`
+  if(erros.length == 0){
+    resp.innerText = "Ok! Senha Valida"
+  } else {
+    resp.innerText = `Erro... A senha deve ${erros.join(", ")}`
+  }
 })
